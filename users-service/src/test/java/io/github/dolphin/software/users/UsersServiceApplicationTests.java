1f0fd7739587b16f32ec386c7d029b0196213cef
Hugo Saldarriaga <hugosmm2000@gmail.com>
2024-02-19 16:00:45 -0500

    feat: Incorporate UsersServiceApplicationTests for context testing
    A new file, UsersServiceApplicationTests.java, has been introduced to facilitate testing for the user service application. This test file employs the SpringBootTest annotation and features a method to ascertain if the application context has been loaded successfully.

---
+++ users-service/src/test/java/io/github/dolphin/software/users/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dolphin.software.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
