9885555cc6bd9d38b967192a429498b4bd6d61b1
Hugo Saldarriaga <hugosmm2000@gmail.com>
2024-02-19 15:40:39 -0500

    feat: Initialize users-service with Gradle configurations
    Create initial setup for users-service with necessary build.gradle.kts settings, including plugins, Java version, repositories, dependencies, etc. The commit includes gradle.properties, gradlew scripts, and settings.gradle.kts, thereby laying the groundwork for the users-service subproject to be constructed and managed using Gradle.

---
+++ users-service/build.gradle.kts
@@ -0,0 +1,55 @@
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+//    id("com.github.johnrengelman.shadow") version ("8.1.1")
+}
+
+group = "io.github.dolphin.software.users"
+version = "0.1.0-alpha-1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+val annotationJetbrains = "24.0.0"
+
+extra["springCloudVersion"] = "2023.0.0"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains:annotations:$annotationJetbrains")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
