b2c71a5bff0bb33c7e15ca82e10793b5607ce44d
Hugo Saldarriaga <hugosmm2000@gmail.com>
2024-02-19 15:34:59 -0500

    refactor: Reduce redundancy in build.gradle.kts
    Details pertaining to group, version, repositories, dependencies, and tasks which are no longer required have been removed from build.gradle.kts. This declutters the file and improves readability.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,22 +2,6 @@ plugins {
     id("java")
 }
 
-group = "io.github.dolphinprojectsoftware"
-version = "0.1.0-alpha-1"
-
 java {
     sourceCompatibility = JavaVersion.VERSION_21
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
